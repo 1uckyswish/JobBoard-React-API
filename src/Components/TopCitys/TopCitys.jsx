ed3ea8060d55837a6d4963438ab2aef0e770d713
Noel Guillen <noelblas274@gmail.com>
2023-09-22 18:34:16 -0400

    location banner done for now

--- src/Components/TopCitys/TopCitys.jsx
+++ src/Components/TopCitys/TopCitys.jsx
@@ -10,12 +10,36 @@ function TopCitys() {
         <p>Top & Perfect location listings</p>
     </div>
     <div class="angry-grid">
-    <div id="item-0">&nbsp;</div>
-    <div id="item-1">&nbsp;</div>
-    <div id="item-2">&nbsp;</div>
-    <div id="item-3">&nbsp;</div>
-    <div id="item-4">&nbsp;</div>
-    <div id="item-5">&nbsp;</div>
+    <div id="item-0">
+      <h2>Los Angeles</h2>
+       <span></span>
+      <p>3351 Listing</p>
+    </div>
+    <div id="item-1">
+       <h2>San Francisco</h2>
+        <span></span>
+      <p>3351 Listing</p>
+    </div>
+    <div id="item-2">
+       <h2>New York</h2>
+        <span></span>
+      <p>3351 Listing</p>
+    </div>
+    <div id="item-3">
+       <h2>Chicago</h2>
+        <span></span>
+      <p>3351 Listing</p>
+    </div>
+    <div id="item-4">
+       <h2>Atlanta</h2>
+        <span></span>
+      <p>3351 Listing</p>
+    </div>
+    <div id="item-5">
+       <h2>London</h2>
+        <span></span>
+      <p>3351 Listing</p>
+    </div>
     </div>
 </div>
   )
