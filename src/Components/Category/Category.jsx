91c6959de3d36c70cd599bb6f133599889ed0577
Noel Guillen <noelblas274@gmail.com>
2023-09-24 19:06:30 -0400

    added grid for category cards

--- src/Components/Category/Category.jsx
+++ src/Components/Category/Category.jsx
@@ -1,11 +1,26 @@
 import React from 'react'
-
+import CategoryCard from '../CategoryCard/CategoryCard'
 import "./Category.css"
 
 function Category() {
+  const array = [1,1,1,1,1,1]
   return (
-    <div>
-      cat
+    <div className='category-card-container'>
+      <h2>Search By Category</h2>
+      <p>Top Jobs based by Category</p>
+        <div class="grid-card">
+  <div id="card-0">&nbsp;</div>
+  <div id="card-1">&nbsp;</div>
+  <div id="card-2">&nbsp;</div>
+  <div id="card-3">&nbsp;</div>
+  <div id="card-4">&nbsp;</div>
+  <div id="card-5">&nbsp;</div>
+  <div id="card-6">&nbsp;</div>
+  <div id="card-7">&nbsp;</div>
+  <div id="card-8">&nbsp;</div>
+</div>
+
+
     </div>
   )
 }
