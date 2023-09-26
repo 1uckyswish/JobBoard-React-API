84eea60c4c1b1c729b897dbb9bba842215868553
Noel Guillen <noelblas274@gmail.com>
2023-09-25 20:06:29 -0400

    added css for category jobs

--- src/Components/Category/Category.jsx
+++ src/Components/Category/Category.jsx
@@ -3,24 +3,103 @@ import CategoryCard from '../CategoryCard/CategoryCard'
 import "./Category.css"
 
 function Category() {
-  const array = [1,1,1,1,1,1]
+ const jobDetails = [
+  {
+    id: 0,
+    cat: "engineering",
+    title: "Software Engineer",
+    time: 'full time',
+    loc: 'San Francisco',
+    buss: 'Facebook',
+    date: 'posted 9.25/23'
+  },
+  {
+    id: 1,
+    cat: "marketing",
+    title: "Digital Marketing Specialist",
+    time: 'part time',
+    loc: 'Los Angeles',
+    buss: 'Amazon',
+    date: 'posted 9.24/23'
+  },
+  {
+    id: 2,
+    cat: "finance",
+    title: "Financial Analyst",
+    time: 'full time',
+    loc: 'Chicago',
+    buss: 'Microsoft',
+    date: 'posted 9.23/23'
+  },
+  {
+    id: 3,
+    cat: "healthcare",
+    title: "Registered Nurse",
+    time: 'contract',
+    loc: 'Houston',
+    buss: 'Apple',
+    date: 'posted 9.22/23'
+  },
+  {
+    id: 4,
+    cat: "education",
+    title: "Elementary School Teacher",
+    time: 'full time',
+    loc: 'Boston',
+    buss: 'Netflix',
+    date: 'posted 9.21/23'
+  },
+  {
+    id: 5,
+    cat: "sales",
+    title: "Sales Representative",
+    time: 'full time',
+    loc: 'Miami',
+    buss: 'Tesla',
+    date: 'posted 9.20/23'
+  },
+  {
+    id: 6,
+    cat: "technology",
+    title: "IT Support Specialist",
+    time: 'part time',
+    loc: 'Seattle',
+    buss: 'Microsoft',
+    date: 'posted 9.19/23'
+  },
+  {
+    id: 7,
+    cat: "healthcare",
+    title: "Physical Therapist",
+    time: 'full time',
+    loc: 'Denver',
+    buss: 'Medtronic',
+    date: 'posted 9.18/23'
+  },
+  {
+    id: 8,
+    cat: "design",
+    title: "Graphic Designer",
+    time: 'contract',
+    loc: 'New York',
+    buss: 'Apple',
+    date: 'posted 9.17/23'
+  }
+];
+
+
+
+
+
   return (
     <div className='category-card-container'>
       <h2>Search By Category</h2>
       <p>Top Jobs based by Category</p>
         <div class="grid-card">
-  <div id="card-0">&nbsp;</div>
-  <div id="card-1">&nbsp;</div>
-  <div id="card-2">&nbsp;</div>
-  <div id="card-3">&nbsp;</div>
-  <div id="card-4">&nbsp;</div>
-  <div id="card-5">&nbsp;</div>
-  <div id="card-6">&nbsp;</div>
-  <div id="card-7">&nbsp;</div>
-  <div id="card-8">&nbsp;</div>
+          {jobDetails.map((job, index) => {
+ return  <CategoryCard key={index} data={job} />
+})}
 </div>
-
-
     </div>
   )
 }
--- src/Components/CategoryCard/CategoryCard.jsx
+++ src/Components/CategoryCard/CategoryCard.jsx
@@ -1,9 +1,26 @@
 import React from 'react';
 import './CategoryCard.css'
+import logo from "../../assets/Img/google.png"
 
-function CategoryCard() {
+function CategoryCard({index, data}) {
   return (
-    <div>CategoryCard</div>
+    <div id={`card-${data.id}`}>
+      <div className="category-title">
+        <img src={logo} />
+        <h3>{data.cat}</h3>
+      </div>
+      <div className="category-job-title">
+        <h2>{data.title}</h2>
+      <div className="category-job-title-details">
+          <p>{data.time}</p>
+      <p>{data.loc}</p>
+      </div>
+      </div>
+    <div className="category-job-date">
+         <h3>{data.buss}</h3>
+      <p>{data.date}</p>
+    </div>
+    </div>
   )
 }
 
