fc1a37b1fa136664b85d4d4d5f4c7d1d98ae4572
Noel Guillen <noelblas274@gmail.com>
2023-09-26 22:20:38 -0400

    images applied for temp code

---
+++ src/utils/logo.js
@@ -0,0 +1,13 @@
+import logo1 from "../../src/assets/Logos/amazon.png";
+import logo2 from "../../src/assets/Logos/apple.png";
+import logo3 from "../../src/assets/Logos/figma.png";
+import logo4 from "../../src/assets/Logos/google.png";
+import logo5 from "../../src/assets/Logos/linkedin.png";
+import logo6 from "../../src/assets/Logos/microsoft.png";
+import logo7 from "../../src/assets/Logos/slack.png";
+import logo8 from "../../src/assets/Logos/spotify.png";
+import logo9 from "../../src/assets/Logos/twitch.png";
+
+
+
+export  const objOfLogos = [logo1,logo2,logo3,logo4,logo5,logo6,logo7,logo8,logo9]
