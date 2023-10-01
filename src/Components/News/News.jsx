0c6dbeb770f453dc1fdda7e33b40e4822ca91114
Noel Guillen <noelblas274@gmail.com>
2023-10-01 15:20:05 -0400

    fetched data

--- src/Components/News/News.jsx
+++ src/Components/News/News.jsx
@@ -1,8 +1,35 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import NewsCard from '../NewsCard/NewsCard'
 import "./News.css"
+import axios from 'axios'
 
 function News() {
+  const [fetchedData, setFetchedData] = useState([]);
+const url = 'https://real-time-news-data.p.rapidapi.com/search?query=Job%20Market&country=US&lang=en';
+const options = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '8d2171684amsh40c452b66155ebfp1c033ajsn3c95ae02c598',
+		'X-RapidAPI-Host': 'real-time-news-data.p.rapidapi.com'
+	}
+};
+
+useEffect(()=>{
+  fetch(url, options)
+  .then(response => response.json())
+  .then(data => {
+    // Handle the data here
+    setFetchedData(data.data)
+  })
+  .catch(error => {
+    // Handle errors here
+    console.error('Error:', error);
+  });
+},[]);
+
+console.log(fetchedData)
+
+
   return (
     <div className='news-container'>
         <div className='news-header'>
