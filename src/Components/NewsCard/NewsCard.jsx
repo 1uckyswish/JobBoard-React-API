7711f285047414985c67dad74920b83e13b6715a
Noel Guillen <noelblas274@gmail.com>
2023-09-30 21:27:04 -0400

    completed UI for news cards and section

--- src/Components/NewsCard/NewsCard.jsx
+++ src/Components/NewsCard/NewsCard.jsx
@@ -5,10 +5,17 @@ import logoImage from "../../assets/Logos/amazon.png"
 function NewsCard() {
   return (
     <div className='news-detail-card'>
-      <img src="https://ichef.bbci.co.uk/news/976/cpsprodpb/1AAA/production/_131262860_ec10df05890093f10ef58569e4a742ed0f44535d0_106_5500_30951000x563.jpg.webp"/>
+      <img src="https://images.barrons.com/im-81261077?width=940&size=1.5005861664712778"/>
      <div className='news-detail-card-title'>
-        <img src={logoImage} />
-        <p>Amazon <span>- 10 hours ago</span></p>
+        <img src='https://upload.wikimedia.org/wikipedia/commons/thumb/6/66/CNN_International_logo.svg/2048px-CNN_International_logo.svg.png' />
+        <p>CNN News <span>- 10 hours ago</span></p>
+     </div>
+     <div className='news-detail-info'>
+      <h2>This Time Really Is Different for the Economy. Just Look at the Job Market’s Confounding Strength.</h2>
+      <p>Since early last year, Fed officials have warned of the labor-market fallout that their attempt to cool prices probably would cause. Fed Chair Jerome Powell called the anticipated pain an “unintended consequence” of the task at hand. </p>
+      <div className='news-detail-category'>
+        <p>Job Market <span>- 1min read</span></p>
+      </div>
      </div>
     </div>
   )
